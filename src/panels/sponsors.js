8f126611ff5bfad057e6eb79127b52110e302929
Kevin Wang <kevin@kevinwang.com>
2016-03-10 13:00:30 -0600

    Add remaining sponsors.

--- src/panels/sponsors.js
+++ src/panels/sponsors.js
@@ -8,16 +8,31 @@ var sponsors = require('../data/sponsors.json');
 var SponsorsPanel = React.createClass({
     render: function() {
         var goldLogos = sponsors.gold.map(function(sponsor) {
-            return <li key={sponsor.name}>
-                <img src={sponsor.logoPath} style={{maxWidth: 300}} />
-            </li>
+            return <div className="sponsor-logo" key={sponsor.name}>
+                <img src={sponsor.logoPath} />
+            </div>
+        });
+        var silverLogos = sponsors.silver.map(function(sponsor) {
+            return <div className="sponsor-logo" key={sponsor.name}>
+                <img src={sponsor.logoPath} />
+            </div>
+        });
+        var bronzeLogos = sponsors.bronze.map(function(sponsor) {
+            return <div className="sponsor-logo" key={sponsor.name}>
+                <img src={sponsor.logoPath} />
+            </div>
         });
         return <div className="panel panel-fill">
             <div className="panel-heading">
                 <h2>Sponsors</h2>
             </div>
             <div className="panel-body">
+                <h3>Gold</h3>
                 {goldLogos}
+                <h3>Silver</h3>
+                {silverLogos}
+                <h3>Bronze</h3>
+                {bronzeLogos}
             </div>
         </div>;
     }
