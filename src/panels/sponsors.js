85f73289a5cb14cde7b0fc39a3cbd1b69afbc783
Kevin Wang <kevin@kevinwang.com>
2016-03-10 14:05:20 -0600

    Sponsor panel styling.

--- src/panels/sponsors.js
+++ src/panels/sponsors.js
@@ -6,33 +6,33 @@ var sponsors = require('../data/sponsors.json');
  * Sponsors panel.
  */
 var SponsorsPanel = React.createClass({
+    getInitialState: function() {
+        return {
+            index: 0
+        };
+    },
+
+    nextSponsor: function() {
+        var newIndex = (this.state.index + 1) % sponsors.length;
+        this.setState({index: newIndex});
+    },
+
+    componentDidMount: function() {
+        setInterval(this.nextSponsor, 5000);
+    },
+
+    getCurrentSponsor: function() {
+        return sponsors[this.state.index];
+    },
+
     render: function() {
-        var goldLogos = sponsors.gold.map(function(sponsor) {
-            return <div className="sponsor-logo" key={sponsor.name}>
-                <img src={sponsor.logoPath} />
-            </div>
-        });
-        var silverLogos = sponsors.silver.map(function(sponsor) {
-            return <div className="sponsor-logo" key={sponsor.name}>
-                <img src={sponsor.logoPath} />
-            </div>
-        });
-        var bronzeLogos = sponsors.bronze.map(function(sponsor) {
-            return <div className="sponsor-logo" key={sponsor.name}>
-                <img src={sponsor.logoPath} />
-            </div>
-        });
+        var sponsor = this.getCurrentSponsor();
         return <div className="panel panel-fill">
             <div className="panel-heading">
                 <h2>Sponsors</h2>
             </div>
-            <div className="panel-body">
-                <h3>Gold</h3>
-                {goldLogos}
-                <h3>Silver</h3>
-                {silverLogos}
-                <h3>Bronze</h3>
-                {bronzeLogos}
+            <div className="panel-body sponsor-body">
+                <img className="sponsor-logo" src={sponsor.logoPath} />
             </div>
         </div>;
     }
