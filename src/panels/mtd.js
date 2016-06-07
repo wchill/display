e8a7c72d46266f1192e4baf1f68c5d3c4e3574da
Kevin Wang <kevin@kevinwang.com>
2016-06-06 22:23:46 -0700

    Add no upcoming departures text.

--- src/panels/mtd.js
+++ src/panels/mtd.js
@@ -1,6 +1,7 @@
 var React = require('react');
 var $ = require('jquery');
 var async = require('async');
+var classnames = require('classnames');
 
 var secrets = require('../secrets');
 
@@ -39,25 +40,36 @@ var MTDPanel = React.createClass({
 
     render: function() {
         var departures = this.state.departures.slice(0, 5);
-        var items = departures.map(function(departure) {
-            var headsign = departure.headsign.split(' ');
-            var num = headsign[0];
-            var name = headsign.slice(1).join(' ');
-            var mins = departure.expected_mins;
-            return <div className="bus-item" key={departure.trip.trip_id}>
-                <div className="bus-name">
-                    <strong>{num}</strong> <small>{name}</small>
-                </div>
-                <div className="bus-mins">{this.getMinsText(mins)}</div>
-            </div>;
-        }.bind(this));
+
+        var body;
+        if (departures.length > 0) {
+            body = departures.map(function(departure) {
+                var headsign = departure.headsign.split(' ');
+                var num = headsign[0];
+                var name = headsign.slice(1).join(' ');
+                var mins = departure.expected_mins;
+                return <div className="bus-item" key={departure.trip.trip_id}>
+                    <div className="bus-name">
+                        <strong>{num}</strong> <small>{name}</small>
+                    </div>
+                    <div className="bus-mins">{this.getMinsText(mins)}</div>
+                </div>;
+            }.bind(this));
+        } else {
+            body = <p>No upcoming departures</p>;
+        }
+
+        var bodyClass = classnames({
+            'panel-body': true,
+            'mtd-body-no-departures': departures.length == 0
+        });
 
         return <div className="panel mtd-panel">
             <div className="panel-heading">
                 <h2>MTD - Goodwin &amp; Main</h2>
             </div>
-            <div className="panel-body">
-                {items}
+            <div className={bodyClass}>
+                {body}
             </div>
         </div>;
     }
